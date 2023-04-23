11b012aab31d6aaf4d65433a0d026a095c778b14
AlexFZR <abadragan@gmail.com>
2023-04-23 15:56:25 +0200

    defined the json structs

--- bookworm/bookworm.go
+++ bookworm/bookworm.go
@@ -2,6 +2,16 @@ package main
 
 import "os"
 
+type Bookworm struct {
+	Name  string `json:"name"`
+	Books []Book `json:"books"`
+}
+
+type Book struct {
+	Author string `json:"author"`
+	Title  string `json:"title"`
+}
+
 func loadBookworms(filePath string) ([]Bookworm, error) {
 	f, err := os.Open(filePath)
 	if err != nil {
