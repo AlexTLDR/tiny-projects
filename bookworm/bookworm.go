899c43d021853937599b648949965e306dcf4278
AlexFZR <abadragan@gmail.com>
2023-04-23 15:49:18 +0200

    creating the loadBookworms func

---
+++ bookworm/bookworm.go
@@ -0,0 +1,12 @@
+package main
+
+import "os"
+
+func loadBookworms(filePath string) ([]Bookworm, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	return nil, nil
+}
