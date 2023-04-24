0ad5e345de92da506c860e19640da0686dcc795c
AlexFZR <abadragan@gmail.com>
2023-04-24 15:00:07 +0200

    buiding the file exists case

--- bookworm/bookworm_internal_test.go
+++ bookworm/bookworm_internal_test.go
@@ -1,13 +1,32 @@
+package main
 
-
-type testCase struct {
-	bookwormsFile string
-	want          []bookworm
-	wantErr       bool
-}
-tests := map[string]testCase{}
+import "testing"
 
 var (
-	forTheLoveOfGo    = Book{Author: "John Arundel", Title: "The Handmaid's Tale"}
-	thePowerOfGoTools = Book{Author: "John Arundel", Title: "The power of Go tools"}
+	forTheLoveOfGo                                   = Book{Author: "John Arundel", Title: "The Handmaid's Tale"}
+	thePowerOfGoTools                                = Book{Author: "John Arundel", Title: "The power of Go tools"}
+	makingThingsHappen                               = Book{Author: "JScott Berkun", Title: "Making things happen"}
+	tjHarvardBusinessReviewProjectManagementHandbook = Book{Author: "JAntonio Nieto-Rodriguez", Title: "TJHarvard Business Review Project Management Handbook"}
 )
+
+// type testCase struct {
+// 	bookwormsFile string
+// 	want          []Bookworm
+// 	wantErr       bool
+// }
+
+func TestLoadBookworms_success(t *testing.T) {
+	tests := map[string]struct {
+		bookwormsFile string
+	want          []Bookworm
+	wantErr       bool
+	}{
+		"file exists": {
+			bookwormsFile: "testdata/bookworms.json",
+			want: []Bookworm{
+				{Name: "Alex", Books: []Book{forTheLoveOfGo,thePowerOfGoTools }},
+				{Name: "Cami", Books: []Book{makingThingsHappen,tjHarvardBusinessReviewProjectManagementHandbook }},
+			},
+		}
+	}
+}
