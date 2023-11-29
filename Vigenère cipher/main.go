b7c342dab4c4902a0faaa1f3ed0e518348ba9e59
AlexTLDR <alex.badragan@protonmail.com>
2023-11-29 10:22:24 +0100

    cipher

--- Vigenère cipher/main.go
+++ Vigenère cipher/main.go
@@ -2,11 +2,22 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	cipherText := "CSOITEUIWUIZNSROCNKFD"
 	keyword := "GOLANG"
+	fmt.Println(decipher(cipherText, keyword))
+
+	message := "your message goes here"
+	keyword = "golang"
+	fmt.Println(cipher(message, keyword))
+}
+
+func decipher(cipherText string, keyword string) string {
 	var message string
 	var keyIndex int
 
@@ -24,5 +35,29 @@ func main() {
 		keyIndex %= len(keyword)
 	}
 
-	fmt.Println(message)
+	return message
+}
+
+func cipher(message string, keyword string) string {
+	var cipherText string
+	var keyIndex int
+
+	message = strings.ToUpper(strings.Replace(message, " ", "", -1))
+	keyword = strings.ToUpper(strings.Replace(keyword, " ", "", -1))
+
+	for i := 0; i < len(message); i++ {
+		// A=0, B=1, ... Z=25
+		m := message[i] - 'A'
+		k := keyword[keyIndex] - 'A'
+
+		// message letter + key letter
+		c := (m+k)%26 + 'A'
+		cipherText += string(c)
+
+		// increment keyIndex
+		keyIndex++
+		keyIndex %= len(keyword)
+	}
+
+	return cipherText
 }
