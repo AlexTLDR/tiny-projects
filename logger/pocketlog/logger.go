24cae9980e3019a35319b4393720a82be3c495d5
AlexTLDR <alex.badragan@protonmail.com>
2025-10-19 18:42:15 +0200

    logf refactor

--- logger/pocketlog/logger.go
+++ logger/pocketlog/logger.go
@@ -2,49 +2,75 @@ package pocketlog
 
 import (
 	"fmt"
+	"io"
+	"os"
 )
 
+// Logger is used to log information.
 type Logger struct {
 	threshold Level
+	output    io.Writer
 }
 
-// Debugf formats and prints a message if the log level is debug or higher
+// Debugf formats and prints a message if the log level is debug or higher.
 func (l *Logger) Debugf(format string, args ...any) {
-	if l.threshold > LevelDebug {
-		return
+	if l.output == nil {
+		l.output = os.Stdout
 	}
-
-	_, _ = fmt.Printf(format+"\n", args...)
+	if l.threshold <= LevelDebug {
+		l.logf("[DEBUG]", format, args...)
+	}
 }
 
 func (l *Logger) Infof(format string, args ...any) {
-	if l.threshold > LevelInfo {
-		return
+	if l.output == nil {
+		l.output = os.Stdout
 	}
-	_, _ = fmt.Printf(format+"\n", args...)
+
+	if l.threshold <= LevelInfo {
+		l.logf("[INFO]", format, args...)
+	}
 }
 
 func (l *Logger) Warnf(format string, args ...any) {
-	if l.threshold > LevelWarn {
-		return
+	if l.output == nil {
+		l.output = os.Stdout
 	}
-	_, _ = fmt.Printf(format+"\n", args...)
+
+	if l.threshold <= LevelWarn {
+		l.logf("[WARN]", format, args...)
+	}
 }
 
 func (l *Logger) Errorf(format string, args ...any) {
-	if l.threshold > LevelError {
-		return
+	if l.output == nil {
+		l.output = os.Stdout
 	}
-	_, _ = fmt.Printf(format+"\n", args...)
+
+	if l.threshold <= LevelError {
+		l.logf("[ERROR]", format, args...)
+	}
 }
 
 func (l *Logger) Fatalf(format string, args ...any) {
-	if l.threshold > LevelFatal {
-		return
+	if l.output == nil {
+		l.output = os.Stdout
 	}
-	_, _ = fmt.Printf(format+"\n", args...)
+
+	if l.threshold > LevelFatal {
+		l.logf("[FATAL]", format, args...)
+	}
 }
 
-func New(threshold Level) *Logger {
-	return &Logger{threshold: threshold}
+func (l *Logger) logf(level string, format string, args ...any) {
+	_, _ = fmt.Fprintf(l.output, level+": "+format+"\n", args...)
+}
+
+// New returns you a logger, ready to log at the required threshold.
+// The default output is Stdout.
+func New(threshold Level, output io.Writer) *Logger {
+	return &Logger{
+		threshold: threshold,
+		output:    output,
+	}
 }
