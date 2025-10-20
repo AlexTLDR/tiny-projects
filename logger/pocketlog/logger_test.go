0da26c53017df83e40254c667ad1bdbd9b41d787
AlexTLDR <alex.badragan@protonmail.com>
2025-10-20 11:45:33 +0200

    fixed debugLogger

--- logger/pocketlog/logger_test.go
+++ logger/pocketlog/logger_test.go
@@ -1,8 +1,12 @@
 package pocketlog_test
 
-import "github.com/AlexTLDR/tiny-projects/logger/pocketlog"
+import (
+	"os"
+
+	"github.com/AlexTLDR/tiny-projects/logger/pocketlog"
+)
 
 func ExampleLogger_debugf() {
-	debugLogger := pocketlog.New(pocketlog.LevelDebug)
+	debugLogger := pocketlog.New(pocketlog.LevelDebug, os.Stdout)
 	debugLogger.Debugf("Hello, %s", "world")
 }
